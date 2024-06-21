b260adbe0d8635e1152c875cfc27f587427553e7
Matthias <matthias.monnier@gmail.com>
2024-06-21 16:20:56 +0200

    Add conditional prod/dev

--- client/dojo.config.ts
+++ client/dojo.config.ts
@@ -1,4 +1,5 @@
-import local from "../contracts/manifests/slot/manifest.json";
+import local from "../contracts/manifests/dev/manifest.json";
+import slot from "../contracts/manifests/slot/manifest.json";
 
 const {
   VITE_PUBLIC_NODE_URL,
@@ -28,6 +29,6 @@ export function dojoConfig() {
     feeTokenAddress:
       VITE_PUBLIC_FEE_TOKEN_ADDRESS ||
       "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-    manifest: VITE_PUBLIC_PRODUCTION ? local : local,
+    manifest: VITE_PUBLIC_PRODUCTION ? slot : local,
   };
 }
