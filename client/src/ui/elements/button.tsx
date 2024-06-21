0751234fe459d6811b99ee547b5216ccb59f90bc
Matthias <matthias.monnier@gmail.com>
2024-06-21 18:07:39 +0200

    Fix build

--- client/src/ui/elements/button.tsx
+++ client/src/ui/elements/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot, Slot } from "@radix-ui/react-slot";
+import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import useSound from "use-sound";
 import hover from "/sounds/effects/hover.wav";
