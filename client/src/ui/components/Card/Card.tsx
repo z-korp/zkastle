0fa497f3358fdcd552b99d3793c4d17b221b3f5b
Matthias <matthias.monnier@gmail.com>
2024-06-25 20:27:31 +0200

    Fix card backface on mobile

--- client/src/ui/components/Card/Card.tsx
+++ client/src/ui/components/Card/Card.tsx
@@ -86,139 +86,151 @@ const Card: React.FC<CardProps> = ({
               style={{ backgroundImage: `url('${card.getImage()}')` }}
             />
           )}
-          <div className="absolute top-0 right-0 px-6 py-3">
-            <p className="font-bold text-black">{label}</p>
-          </div>
+          {!isFlipped && (
+            <>
+              <div className="absolute top-0 right-0 px-6 py-3">
+                <p className="font-bold text-black">{label}</p>
+              </div>
 
-          {/* Score */}
-          {!!score && (
-            <div className="absolute top-0 left-0 p-3 flex gap-1 items-center">
-              <p className="font-bold text-black">{score}</p>
-              <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
-            </div>
-          )}
-
-          {/* Upgrade */}
-          {!!upgrade && (
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 p-3 flex gap-1 justify-center items-center">
-              {Array.from({ length: upgrade }).map((_, index) => (
-                <FontAwesomeIcon
-                  key={index}
-                  icon={faKhanda}
-                  className="text-slate-900 h-3"
-                />
-              ))}
-            </div>
-          )}
-
-          {/* Foreground Description */}
-          <div
-            className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10`}
-          >
-            <p className="text-lg z-10 text-black">{fgDescription}</p>
-          </div>
-
-          {/* Resources */}
-          <div
-            className={`absolute bottom-[60%] left-1/2 -translate-x-1/2 z-10`}
-          >
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <div className={`${stored && "opacity-50"}`}>
-                    <Resource resource={resource} />
-                  </div>
-                </TooltipTrigger>
-                {stored && (
-                  <TooltipContent className="text-black translate-y-8">
-                    Already stored
-                  </TooltipContent>
-                )}
-              </Tooltip>
-            </TooltipProvider>
-          </div>
-
-          {/* Actions */}
-          <div className="flex flex-col justify-between h-full pt-8 pb-4">
-            <Badge className="flex mx-auto text z-10">{card.value}</Badge>
-            <div className="flex flex-col gap-2 items-start z-10 ml-2">
-              {/* Store */}
-              {card.isAllowed(side, new Action(ActionType.Store)) && (
-                <div className="flex gap-2 items-center">
-                  <Store
-                    choice={first}
-                    enabled={actionable && !stored}
-                    costs={card.getCost(side, new Action(ActionType.Store))}
-                  />
-                  <Cost
-                    resources={card.getCost(side, new Action(ActionType.Store))}
-                  />
+              {/* Score */}
+              {!!score && (
+                <div className="absolute top-0 left-0 p-3 flex gap-1 items-center">
+                  <p className="font-bold text-black">{score}</p>
+                  <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
                 </div>
               )}
 
-              {/* Rotate */}
-              {!noButton &&
-                card.isAllowed(side, new Action(ActionType.Rotate)) && (
-                  <div className="flex gap-2 items-center">
-                    <div
-                      onMouseEnter={() => onMouseEnter(ActionType.Rotate)}
-                      onMouseLeave={() => resetUpgradeToShow()}
-                    >
-                      <Rotate
-                        newSide={
-                          new Side(side.update(new Action(ActionType.Rotate)))
-                        }
+              {/* Upgrade */}
+              {!!upgrade && (
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 p-3 flex gap-1 justify-center items-center">
+                  {Array.from({ length: upgrade }).map((_, index) => (
+                    <FontAwesomeIcon
+                      key={index}
+                      icon={faKhanda}
+                      className="text-slate-900 h-3"
+                    />
+                  ))}
+                </div>
+              )}
+
+              {/* Foreground Description */}
+              <div
+                className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10`}
+              >
+                <p className="text-lg z-10 text-black">{fgDescription}</p>
+              </div>
+
+              {/* Resources */}
+              <div
+                className={`absolute bottom-[60%] left-1/2 -translate-x-1/2 z-10`}
+              >
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div className={`${stored && "opacity-50"}`}>
+                        <Resource resource={resource} />
+                      </div>
+                    </TooltipTrigger>
+                    {stored && (
+                      <TooltipContent className="text-black translate-y-8">
+                        Already stored
+                      </TooltipContent>
+                    )}
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+
+              {/* Actions */}
+              <div className="flex flex-col justify-between h-full pt-8 pb-4">
+                <Badge className="flex mx-auto text z-10">{card.value}</Badge>
+                <div className="flex flex-col gap-2 items-start z-10 ml-2">
+                  {/* Store */}
+                  {card.isAllowed(side, new Action(ActionType.Store)) && (
+                    <div className="flex gap-2 items-center">
+                      <Store
                         choice={first}
-                        enabled={actionable}
-                        costs={card.getCost(
+                        enabled={actionable && !stored}
+                        costs={card.getCost(side, new Action(ActionType.Store))}
+                      />
+                      <Cost
+                        resources={card.getCost(
                           side,
-                          new Action(ActionType.Rotate),
+                          new Action(ActionType.Store),
                         )}
                       />
                     </div>
+                  )}
 
-                    <Cost
-                      resources={card.getCost(
-                        side,
-                        new Action(ActionType.Rotate),
-                      )}
-                    />
-                  </div>
-                )}
+                  {/* Rotate */}
+                  {!noButton &&
+                    card.isAllowed(side, new Action(ActionType.Rotate)) && (
+                      <div className="flex gap-2 items-center">
+                        <div
+                          onMouseEnter={() => onMouseEnter(ActionType.Rotate)}
+                          onMouseLeave={() => resetUpgradeToShow()}
+                        >
+                          <Rotate
+                            newSide={
+                              new Side(
+                                side.update(new Action(ActionType.Rotate)),
+                              )
+                            }
+                            choice={first}
+                            enabled={actionable}
+                            costs={card.getCost(
+                              side,
+                              new Action(ActionType.Rotate),
+                            )}
+                          />
+                        </div>
 
-              {/* Flip */}
-              {!noButton &&
-                card.isAllowed(side, new Action(ActionType.Flip)) && (
-                  <div className="flex gap-2 items-center">
-                    <div
-                      onMouseEnter={() => onMouseEnter(ActionType.Flip)}
-                      onMouseLeave={() => resetUpgradeToShow()}
-                    >
-                      <Flip
-                        newSide={
-                          new Side(side.update(new Action(ActionType.Flip)))
-                        }
-                        choice={first}
-                        enabled={actionable}
-                        costs={card.getCost(side, new Action(ActionType.Flip))}
-                      />
-                    </div>
-                    <Cost
-                      resources={card.getCost(
-                        side,
-                        new Action(ActionType.Flip),
-                      )}
-                    />
-                  </div>
-                )}
+                        <Cost
+                          resources={card.getCost(
+                            side,
+                            new Action(ActionType.Rotate),
+                          )}
+                        />
+                      </div>
+                    )}
 
-              {/* Discard */}
-              {!noButton &&
-                card.isAllowed(side, new Action(ActionType.Discard)) && (
-                  <Discard enabled={first} />
-                )}
-            </div>
-          </div>
+                  {/* Flip */}
+                  {!noButton &&
+                    card.isAllowed(side, new Action(ActionType.Flip)) && (
+                      <div className="flex gap-2 items-center">
+                        <div
+                          onMouseEnter={() => onMouseEnter(ActionType.Flip)}
+                          onMouseLeave={() => resetUpgradeToShow()}
+                        >
+                          <Flip
+                            newSide={
+                              new Side(side.update(new Action(ActionType.Flip)))
+                            }
+                            choice={first}
+                            enabled={actionable}
+                            costs={card.getCost(
+                              side,
+                              new Action(ActionType.Flip),
+                            )}
+                          />
+                        </div>
+                        <Cost
+                          resources={card.getCost(
+                            side,
+                            new Action(ActionType.Flip),
+                          )}
+                        />
+                      </div>
+                    )}
+
+                  {/* Discard */}
+                  {!noButton &&
+                    card.isAllowed(side, new Action(ActionType.Discard)) && (
+                      <Discard enabled={first} />
+                    )}
+                </div>
+              </div>
+            </>
+          )}
         </div>
 
         {/* Background description */}
